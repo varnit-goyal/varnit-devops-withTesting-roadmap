0b139f4af404314970df6f1d36723ce95bbaf664
Varnit Goyal <varnit.goyal103@gmail.com>
2025-09-05 12:58:43 +0530

    First Commit

---
+++ src/main/java/pageObjects/LoginPageObjects.java
@@ -0,0 +1,30 @@
+package pageObjects;
+
+
+import org.openqa.selenium.By;
+
+import testBase.DriverFactory;
+import testBase.TestBase;
+
+public class LoginPageObjects extends TestBase  {
+
+	By EMAIL = By.name("user-name");
+	By PASSWORD = By.name("password");
+	
+	By LOGIN_BTN = By.name("login-button");
+	//By LOGIN_BTN = By.xpath("//button[@type='submit' and text()='Login ']");
+
+
+	//login to App
+	public void login(String email, String password) {
+		sendKeys_custom(DriverFactory.getInstance().getDriver().findElement(EMAIL), "LoginEmailFIeld", email);
+		sendKeys_custom(DriverFactory.getInstance().getDriver().findElement(PASSWORD), "LoginPasswordFIeld", password);
+
+		click_custom(DriverFactory.getInstance().getDriver().findElement(LOGIN_BTN), "LoginButton");
+
+	}
+
+
+}
+
+
